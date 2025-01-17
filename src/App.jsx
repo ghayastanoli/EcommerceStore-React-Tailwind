3afa3707c2e20220b632d266c2bf24ec0a270bd4
[ghayastanoli] <[ghiyastanoli@yahoo.com]>
2025-01-17 16:58:26 +0500

    chore: filter-tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
-      <Route path="/all-products" element={<AllProducts/>} />
+      <Route path="https://ecommerce-store-react-tailwind.vercel.app/all-products" element={<AllProducts/>} />
     </Route>
   ) 
 );
